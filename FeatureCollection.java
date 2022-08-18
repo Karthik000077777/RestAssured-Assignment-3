5e382f6b803c55962e99e3a328a92a125a649ed2
Karthik000077777 <95902811+Karthik000077777@users.noreply.github.com>
2022-08-18 17:04:27 +0530

    Add files via upload

---
+++ FeatureCollection.java
@@ -0,0 +1,17 @@
+package com.kenihin;
+
+import java.util.List;
+
+public class FeatureCollection {
+	
+	private String type;
+	private List<Features> f1;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
